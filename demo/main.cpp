99dd3c601f668a9a3bee7d98111106db873ca3c2
morgan <marymorgan7815@gmail.com>
2021-11-22 01:10:14 +0300

    re4ctygv

--- demo/main.cpp
+++ demo/main.cpp
@@ -9,6 +9,6 @@ int main(int argc, char** argv)
  nlohmann::json data = takeJson(argc, argv);
  std::vector<Student> students = parsingJson(data);
  print(students, std::cout);
-// std::cout << argv[1] << std::endl;
+ //std::cout << argv[1] << std::endl;
 }
 
--- tests/test.cpp
+++ tests/test.cpp
@@ -19,24 +19,24 @@
 TEST(printTest, StudentItem)
 {
   std::stringstream ss;
-  Student s("test", (std::string)"test", (size_t)3, std::vector<std::string>());
+  Student s("test", (std::string)"test", (size_t)4, std::vector<std::string>());
   print(s, ss);
-  ASSERT_EQ("| test                | test      | 3         | 0         |\n"
+  ASSERT_EQ("| test                | test      | 4         | 0         |\n"
       "|---------------------|-----------|-----------|-----------|\n",  ss.str());
 }
 TEST(printTest, StudentsArray) {
     std::stringstream ss;
     std::vector<Student> students;
     students.emplace_back("test", (std::string)"test",
-                          (size_t)3, std::vector<std::string>());
-    students.emplace_back("test", (size_t)4,
-                          (double)3.33, std::vector<std::string>());
+                          (size_t)4, std::vector<std::string>());
+    students.emplace_back("test", (size_t)2,
+                          (double)4.33, std::vector<std::string>());
     print(students, ss);
     ASSERT_EQ("| name                | group     | avg       | debt      |\n"
         "|---------------------|-----------|-----------|-----------|\n"
-        "| test                | test      | 3         | 0         |\n"
+        "| test                | test      | 4         | 0         |\n"
         "|---------------------|-----------|-----------|-----------|\n"
-        "| test                | 4         | 3.33      | 0         |\n"
+        "| test                | 2         | 4.33      | 0         |\n"
         "|---------------------|-----------|-----------|-----------|\n",  ss.str());
 }
 TEST(printTest, json)
@@ -44,16 +44,16 @@ TEST(printTest, json)
   std::string jsonData = "{\n"
       "  \"items\": [\n"
       "    {\n"
-      "      \"name\": \"Akakiy Akaki\",\n"
-      "      \"group\": \"UI8-32\",\n"
+      "      \"name\": \"Vera Voronova\",\n"
+      "      \"group\": \"33\",\n"
       "      \"avg\": \"5.25\",\n"
       "      \"debt\": null\n"
       "    },\n"
       "    {\n"
-      "      \"name\": \"Ded Asambler\",\n"
+      "      \"name\": \"Peter Parker\",\n"
       "      \"group\": 32,\n"
-      "      \"avg\": 3.75,\n"
-      "      \"debt\": \"asembler\"\n"
+      "      \"avg\": 2.75,\n"
+      "      \"debt\": \"GoLang\"\n"
       "    }\n"
       "  ],\n"
       "  \"_meta\": {\n"
@@ -66,9 +66,9 @@ TEST(printTest, json)
   print(students_parsed, ss);
   ASSERT_EQ("| name                | group     | avg       | debt      |\n"
       "|---------------------|-----------|-----------|-----------|\n"
-      "| Akakiy Akaki        | UI8-32    | 5.25      | null      |\n"
+      "| Vera Voronova       | 33        | 5.25      | null      |\n"
       "|---------------------|-----------|-----------|-----------|\n"
-      "| Ded Asambler        | 32        | 3.75      | asembler  |\n"
+      "| Peter Parker        | 32        | 2.75      | GoLang    |\n"
       "|---------------------|-----------|-----------|-----------|\n", ss.str());
 }
 
@@ -77,16 +77,16 @@ TEST(parseTest, jsonParse)
   std::string jsonData = "{\n"
       "  \"items\": [\n"
       "    {\n"
-      "      \"name\": \"Akakiy Akaki\",\n"
-      "      \"group\": \"UI8-32\",\n"
-      "      \"avg\": \"5.25\",\n"
+      "      \"name\": \"Vera Voronova\",\n"
+      "      \"group\": \"33\",\n"
+      "      \"avg\": \"4.25\",\n"
       "      \"debt\": null\n"
       "    },\n"
       "    {\n"
-      "      \"name\": \"Ded Asambler\",\n"
-      "      \"group\": 32,\n"
-      "      \"avg\": 3.75,\n"
-      "      \"debt\": \"asembler\"\n"
+      "      \"name\": \"Peter Parker\",\n"
+      "      \"group\": 34,\n"
+      "      \"avg\": 2.25,\n"
+      "      \"debt\": \"GoLang\"\n"
       "    }\n"
       "  ],\n"
       "  \"_meta\": {\n"
@@ -96,10 +96,10 @@ TEST(parseTest, jsonParse)
     nlohmann::json _json = json::parse(jsonData);
     std::vector<Student> students_parsed = parsingJson(_json);
     std::vector<Student> student_inited = {
-        Student("Akakiy Akaki", (std::string)"UI8-32",
-                (std::string)"5.25", nullptr),
-        Student("Ded Asambler", (size_t)32,
-                (double)3.75, (std::string)"asembler")
+        Student("Vera Voronova", (std::string)"33",
+                (std::string)"4.25", nullptr),
+        Student("Peter Parker", (size_t)34,
+                (double)2.25, (std::string)"GoLang")
     };
     ASSERT_EQ(student_inited, students_parsed);
 }
@@ -108,20 +108,20 @@ TEST(parseTest, fromFile)
   std::string jsonData = "{\n"
       "  \"items\": [\n"
       "    {\n"
-      "      \"name\": \"Akakiy Akaki\",\n"
-      "      \"group\": \"2\",\n"
-      "      \"avg\": \"5.25\",\n"
+      "      \"name\": \"Vera Voronova\",\n"
+      "      \"group\": \"33\",\n"
+      "      \"avg\": \"4.25\",\n"
       "      \"debt\":  [\n"
       "        \"Java\",\n"
       "        \"C++\",\n"
-      "        \"Games\"\n"
+      "        \"Python\"\n"
       "      ]\n"
       "    },\n"
       "    {\n"
-      "      \"name\": \"Ded Asembler\",\n"
-      "      \"group\": 32,\n"
-      "      \"avg\": 3.75,\n"
-      "      \"debt\": \"asembler\"\n"
+      "      \"name\": \"Peter Parker\",\n"
+      "      \"group\": 34,\n"
+      "      \"avg\": 2.25,\n"
+      "      \"debt\": \"GoLang\"\n"
       "    }\n"
       "  ],\n"
       "  \"_meta\": {\n"
@@ -153,20 +153,4 @@ TEST(errorCheck, lessArgsTest)
   }
 }
 
-TEST(errorCheck, _metaCheck)
-{
-  try {
-    std::string path = JSON_DIR;
-    path+="/test2.json";
-    char* argv[] ={(char*)"", (char*)(path.c_str())};
-    nlohmann::json json2 = takeJson(2, argv);
-    FAIL() << "Expected: _meta value does not match the array size";
-  }
-  catch(std::runtime_error const & err) {
-    EXPECT_EQ(err.what(),std::string("_meta value does not match the array size"));
-  }
-  catch(...) {
-    FAIL() << "Expected: _meta value does not match the array size";
-  }
-}
 #endif // TEST_CPP_
